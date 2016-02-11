aadbcb29cc0d8e790ad53cabbc316d23cfbc1248
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-02-11 19:20:30 +0000

    update styling to group page

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -3,8 +3,7 @@ class Gift < ActiveRecord::Base
 	belongs_to :group
 	belongs_to :user
 
-	# attr_accessible :avatar
   	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  	validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	
 end
